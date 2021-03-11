7487b7005394047d3bc552841629942a7ee20d7d
escaping-blurred-reality <80349103+escaping-blurred-reality@users.noreply.github.com>
2021-03-11 18:58:27 +0530

    Add files via upload

---
+++ tut4.js
@@ -0,0 +1,24 @@
+let myarr=[25,23,22,24,24.5];
+console.log(myarr);
+myarr.push(30);
+console.log(myarr);
+myarr.pop();
+console.log(myarr);
+myarr.unshift(23.5);
+console.log(myarr);
+myarr.shift();
+console.log(myarr);
+console.log(myarr.length);
+myarr.reverse();
+console.log(myarr);
+marks=myarr.concat(24.5,25);
+console.log(marks);
+let myobj={
+    na:"Riya",
+    rollno: '19BCP160',
+    passion: 'Softwares',
+    profession: 'SDE at Microsoft',
+    basePay: '20LPA',
+   // larr=[1,2,3,4,5]
+}
+console.log(myobj.rollno);
